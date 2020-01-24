5ff0ede1bcab4c9ca2ca2a8632bf3290b0307f08
MahaAmin <mahaamin97@gmail.com>
2020-01-25 00:54:57 +0200

    Basic createDB

--- createDB.sh
+++ createDB.sh
@@ -10,4 +10,8 @@ createDB(){
 }
 
 newDB=$(createDB)
-mkdir Databases/$newDB
+if mkdir Databases/$newDB 2>> log.out; then
+    zenity --notification --text "$newDB created sucessfully\n";
+else 
+    zenity --error --width=300 --height=150 --text "Can not create $newDB. Check log.out for more details.\n";
+fi
