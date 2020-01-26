efb4aaa0fdc0d349cdbdb91b4054ce6109ac816b
aya morsi <ayamorsi163@gmail.com>
2020-01-26 18:25:43 +0200

    delete db

--- deleteDB.sh
+++ deleteDB.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+cd Databases/
+echo "Enter Database Name"
+read name 
+if [ -d  "$name" ]; then 
+    rm -r $name
+    echo "$name database  deleted Successfully"
+  else 
+    echo "No such Database"
+fi
 
-echo "In deleteDB"
+
+    
+
+
