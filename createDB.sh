4bac44e986d5136bc6b1856e8bbcca68009fd0b3
MahaAmin <mahaamin97@gmail.com>
2020-01-29 13:04:52 +0200

    Create Data/ and Metadata/ inside DB directory

--- createDB.sh
+++ createDB.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # TO-HANDLE: User enter multiple names or name with spaces
+# TO-DO: when creating db create 2 sub-directories (Data, Metadata)
 
 echo "Enter Database Name: ";
 read newDB;
 if mkdir Databases/$newDB 2>> log.out; then
+    mkdir Databases/$newDB/Data;
+    mkdir Databases/$newDB/Metadata;
     printf "$newDB created succesfully.\n";
 else
     printf "Can not create $newDB. Check log.out for more details.\n";
