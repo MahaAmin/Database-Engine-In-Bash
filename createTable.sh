ab74928380889d0468dfd7a724af41be470924eb
MahaAmin <mahaamin97@gmail.com>
2020-01-31 14:39:06 +0200

    steps of create table

--- createTable.sh
+++ createTable.sh
@@ -35,4 +35,14 @@ fi
 
 
 # TO-DO: create table function
-# 
+
+# Steps of creating new table
+# 1- Validate table name (noSapces, contains at least one letter, does not start with number)
+# 2- Check if table already exists
+# 3- Create 2 files Data/tableName and MetaData/tableName.metadata
+# 4- Read number of columns from user 
+# 5- For each column :
+#   5.1- Read column name from user
+#   5.2- Let user choose datatype (String/Number)
+#   5.3- Is it primary-key column (Y/N)
+
