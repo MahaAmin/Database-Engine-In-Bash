7f122d32c12f105dea9a0539a9cca0be4d9483aa
MahaAmin <mahaamin97@gmail.com>
2020-01-31 14:53:42 +0200

    Fix quit in mainmenu

--- mainMenu.sh
+++ mainMenu.sh
@@ -12,7 +12,7 @@ do
 		"Create New Database") . ./createDB.sh; break;;
 		"Use Database") . ./useDB.sh; break;;
 		"Delete Database") . ./deleteDB.sh; break;;
-		"Quit") break;;
+		"Quit") break 2;;
 		*) echo "Invalid option $REPLY";;
 	esac
 done
