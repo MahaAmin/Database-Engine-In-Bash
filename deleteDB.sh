95e9d7414cfdf3fcbf676b93669625d93a83d2ba
aya morsi <ayamorsi163@gmail.com>
2020-02-01 09:37:42 +0200

    delete table

--- deleteDB.sh
+++ deleteDB.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-cd Databases/
+
 echo "Enter Database Name"
 read name 
-if [ -d  "$name" ]; then 
-    rm -r $name
+if [ -d  "Databases/$name" ]; then 
+    rm -r Databases/$name
     echo "$name database  deleted Successfully"
   else 
     echo "No such Database"
 fi
-
-
-    
-
 
--- mainMenu.sh
+++ mainMenu.sh
@@ -5,15 +5,15 @@
 options=("Show Databases" "Create New Database" "Use Database" "Delete Database" "Quit")
 while [[ "$option" != "Quit" ]] 
 do
-select option in "${options[@]}"
-do
-	case $option in
-		"Show Databases") . ./showDB.sh; break;;
-		"Create New Database") . ./createDB.sh; break;;
-		"Use Database") . ./useDB.sh; break;;
-		"Delete Database") . ./deleteDB.sh; break;;
-		"Quit") break 2;;
-		*) echo "Invalid option $REPLY";;
-	esac
-done
+	select option in "${options[@]}"
+	do
+		case $option in
+			"Show Databases") . ./showDB.sh; break ;;
+			"Create New Database"). ./createDB.sh; break ;;
+			"Use Database") . ./useDB.sh break ;;
+			"Delete Database") . ./deleteDB.sh; break ;;
+			"Quit")  break;;
+			*) echo "Invalid option $REPLY";;
+		esac
+	done
 done
