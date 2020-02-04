e0f1417ca9a694748230e669731847b276cd5895
MahaAmin <mahaamin97@gmail.com>
2020-02-04 11:08:26 +0200

    Fix features

--- updateTable.sh
+++ updateTable.sh
@@ -2,15 +2,13 @@
 
 
 function updateTable_main_menu(){
-    options=("Add New Column" "Delete Column" "Update Cell Value" "Delete Record" "Return To Main Menu");
+    options=("Delete Column" "Delete Record" "Return To Main Menu");
         while [[ "$option" != "Return To Main Menu" ]] 
     do
     select option in "${options[@]}"
     do
         case $option in
-            "Add New Column") . ./addNewColumn.sh;break ;;
             "Delete Column") . ./deleteColumn.sh; break;;
-            "Update Cell Value") . ./updateCell.sh; break;;
             "Delete Record") . ./deleteRecord.sh; break ;;
             "Return To Main Menu") ./mainMenu.sh; break ;;
             *) echo "Invalid option $REPLY";;
